5b4b2b4663e70996ec95b11e1a1ae6f581b52de8
dudumama-pen <229634712+dudumama-pen@users.noreply.github.com>
2025-10-01 20:55:01 +0300

    used 4 days in this homework

---
+++ labs/lab7/script.js
@@ -0,0 +1,33 @@
+let firstName = "qianwen";
+let newSentence = "my name is" + firstName
+console.log(newSentence)
+console.log("hello i am qianwen");
+
+let num1 = 6;
+let num2 = 3;
+let result = num1 + num2;
+console.log(result);
+
+
+let mixedresult = firstName + num1
+console.log(mixedresult);
+
+
+console.log("hello im qianwen, sdlhiwe qianwen, sweerrrlhiwe qianwen")
+let anotherName = "xiaowang";
+console.log("hello im" +anotherName,"sdlhiwe" +anotherName,"sweerrrlhiwe" +anotherName)
+console.log(`hello im ${anotherName}, sdlhiwe ${anotherName}, sweerrrlhiwe ${anotherName}`)
+
+let num3;
+const num7 =3
+
+num3 = 454;
+
+let a = 12;
+let b = 3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
